c0d6ce7de8ce943c8cf1edbcbaa7aca3e03c1f19
vkrich <vladislav.krichevsky@mail.ru>
2020-10-30 08:53:42 +0000

    Remove incorrect test, cause input data from server can change.

--- test.py
+++ test.py
@@ -13,12 +13,6 @@ class TestCases(unittest.TestCase):
         result = get_request(argparse.Namespace(mode='r_wo', count=2, country='ru', user_id=None))
         self.assertEqual(result, 0)
 
-    def test_get_request_2(self):
-        result = get_request(argparse.Namespace(mode='r_wo', count=1, country=None, user_id=None))
-        self.assertEqual(result, [{'name': '2z Silencep', 'country': 'nl', 'match_type': 2,
-                                   'rating': '2232', 'rank_tier': 40, 'rank_position': 1, 'match_count': 81,
-                                   'match_wins': 66}])
-
 
 if __name__ == '__main__':
     unittest.main()
